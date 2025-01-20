95a76ec1275398095dfe5e9fcda6de05db3e5665
Zivic <djzivic@gmail.com>
2025-01-20 22:12:26 +0100

    add csproj and sln files

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,61 @@
+using NLog;
+using WebApplication1.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//startup (old version)
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+//Configure(old version) //IOC CONTAINER
+builder.Services.AddOpenApi();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureSqlContext(builder.Configuration);//Configuration ?
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.UseCors();
+app.UseHttpsRedirection();
+app.MapControllers();
+
+//do we need this?
+app.UseRouting();
+app.UseAuthorization();
+//app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast2", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
